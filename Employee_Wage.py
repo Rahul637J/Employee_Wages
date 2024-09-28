6c892c6b70953bbe2faf1010dcd3ec5b1f196073
Your Name <you@example.com>
2024-09-28 21:43:42 +0530

    [Rahul] Add: Python program to calculate employee monthly wages based on condition

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -7,73 +7,97 @@
 '''
 
 import random
- 
-WORKING_DAYS_PER_MONTH = 20
-WAGE_PER_HOUR = 20
-FULL_TIME_HOUR = 8
-PART_TIME_HOUR = 4
+
+MAXIMUM_WORKING_HOURS=100
+MAXIMUM_WORKING_DAYS=20
+WAGE_PER_HOUR=20
+FULL_TIME_HOUR=8
+PART_TIME_HOUR=4
 
 def attendance():
     
     '''
-     Description: 
-          The function to check the employee is present or absent 
-     Parameters:
-          None        
-     Return:
-          1 (1): If the employee is present
-          0 (0): If the employee is absent
-     '''
-     
+    Description: 
+        The function to check if the employee is present, part-time, or absent.
+        
+    Parameters:
+        None        
+        
+    Return:
+        1 (int): If the employee is present full-time.
+        2 (int): If the employee is working part-time.
+        0 (int): If the employee is absent.
+    '''
+    
     return random.randint(0, 2)
 
 def calculate_wage_for_day(status):
     
-     '''
-     Description: 
-          The function to calculate employee daily wage based on attendance status
-     Parameters:
-          status (int): The employee's attendance status (Present/PartTime/Absent)
-     Return:
-          wages (int): The daily wage of the employee
-     '''
-     
-     if status == 1:
-        return FULL_TIME_HOUR * WAGE_PER_HOUR, "FullTime"
+    '''
+    Description: 
+        The function to get the daily wage and hours worked based on the employee's status.
+        
+    Parameters:
+        status (int): The employee's attendance status (0,1,2).
+        
+    Return:
+        wages (int): The daily wage of the employee.
+        hours_worked (int): The number of hours worked by the employee that day.
+    '''
+    
+    if status == 1:
+        return FULL_TIME_HOUR * WAGE_PER_HOUR, FULL_TIME_HOUR
+    
+    elif status == 2:
+        return PART_TIME_HOUR * WAGE_PER_HOUR, PART_TIME_HOUR
+    
+    else:
+        return 0, 0
 
-     elif status == 2:
-        return PART_TIME_HOUR * WAGE_PER_HOUR, "PartTime"
-
-     else:
-        return 0, "Absent"
-
-def calculate_monthly_wage():
+def calculate_wage_for_month():
     
     '''
-     Description: 
-          The function to calculate employee monthly wage 
-     Parameters:
-          None
-     Return:
-          total_wage (int): The total monthly wage of the employee
-     '''
+    Description: 
+        The function to calculate the employee's total monthly wage based on daily attendance.
+        
+    Parameters:
+        None
+        
+    Return:
+        monthly_wage (int): The total wage earned by the employee for the month.
+        tot_working_days (int): The total number of days worked.
+        tot_working_hours (int): The total number of hours worked.
+    '''
     
-    total_wage = 0
+    day_wise_wage = {}
+    tot_working_hours, tot_working_days, monthly_wage = 0, 0, 0
     
-    for day in range(WORKING_DAYS_PER_MONTH):
+    while tot_working_hours < MAXIMUM_WORKING_HOURS and tot_working_days < MAXIMUM_WORKING_DAYS:
+        
         status = attendance()
-        daily_wage,status = calculate_wage_for_day(status)
-        total_wage += daily_wage
-    
-    return total_wage
+        daily_wage, working_hours = calculate_wage_for_day(status)
+        
+        key = 'DAY_' + str(tot_working_days+1)
+        day_wise_wage[key] = daily_wage
+
+        # Calculating the wages
+        monthly_wage += daily_wage
+        tot_working_hours += working_hours
+
+        tot_working_days+=1
+
+    return monthly_wage, tot_working_days, tot_working_hours, day_wise_wage
 
 def main():
     
-    print("****Welcome to Employee Wage Computation Program****")
+    print("\n"+"**** Welcome to Employee Wage Computation Program ****"+"\n")
+
+    monthly_wage, tot_working_days, tot_working_hours, day_wise_wage = calculate_wage_for_month()
     
-    # CALCULATE MONTHLY WAGE
-    monthly_wage = calculate_monthly_wage()
-    print(f"The employee's total monthly wage: {monthly_wage}")
-    
+    print(f"Your monthly wage is: RS {monthly_wage}")
+    print(f"Total working days: {tot_working_days}")
+    print(f"Total working hours: {tot_working_hours}")
+    print("Day wise wages :\n",day_wise_wage,"\n")
+
 if __name__ == "__main__":
     main()
