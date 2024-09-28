02b0ca5389d5542d6c10e176b65ea73e3ee4d43a
Your Name <you@example.com>
2024-09-28 21:39:50 +0530

    [Rahul] Add: Python program using switch to calculate employee wages

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -28,34 +28,37 @@ def attendance():
 
 def calculate_wage_for_day(status):
     
-    '''
+     '''
      Description: 
           The function to calculate employee daily wage 
      Parameters:
-          work_hour (int): The work hour for the day
+          status (int): The employee is present or parttime or absent
      Return:
           wages (int): The daily wage of the employee
      '''
-
-    if status == 1:
-        return WAGE_PER_HOUR * FULL_TIME_HOUR, "FullTime"
-
-    elif status == 2:
-        return WAGE_PER_HOUR * PART_TIME_HOUR, "PartTime" 
-
-    else:
-        return 0, "Absent"
+     
+     match status:
+         
+      case 1:
+          return FULL_TIME_HOUR * WAGE_PER_HOUR, "FullTime"
+  
+      case 2:
+          return PART_TIME_HOUR * WAGE_PER_HOUR, "PartTime"
+  
+      case 0:
+            return 0,"Absent"
 
 def main():
+    
     print("****Welcome to Employee Wage Computation Program****")
 
      #  EMPLOYEE ATTEDANCE
     status=attendance()
-    
-     #  CALCULATE DAILY WAGE
-    daily_wage,status=calculate_wage_for_day(status)
-    print(f"Employee status: {status}")
-    print(f"The employee daily wage: {daily_wage}")   
+
+     #  DAILY WAGE
+    wage,status=calculate_wage_for_day(status)
+    print(f'Employee status: {status}')
+    print(f"The daily wage: {wage}")
 
 if __name__ == "__main__":
     main()
